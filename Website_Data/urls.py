7450c17e3273533e3add0bd2df2814537f38a55a
Stacey Melnick <stacey.melnick.np@gmail.com>
2025-12-25 12:11:10 -0800

    updated top message

--- Website_Data/urls.py
+++ Website_Data/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('homepagetext', HomePageTextAPI.as_view(), name='home-page-text')
+    path('homepagetext/', HomePageTextAPI.as_view(), name='home-page-text'),
+    path('homepagetext/<int:id>/', HomePageTextAPI.as_view(), name='home-page-text-delete')
 ]
--- Website_Data/views.py
+++ Website_Data/views.py
@@ -7,18 +7,41 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 
 
-# Create your views here.
 class HomePageTextAPI(APIView):
     
     def get(self, request, *args, **kwargs):
+
         TextItems = HomePageText.objects.all()
         serializer = HomePageTextSerializer(TextItems, many=True)
         return Response(serializer.data)
     
     def post(self, request,*args, **kwargs):
+
         serializer = HomePageTextSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request,id,*args, **kwargs):
+
+        try:
+            thisRecord = HomePageText.objects.get(id=id)
+            thisRecord.delete()
+        except:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({}, status=status.HTTP_200_OK)
+    
+    def patch(self, request,id,*args, **kwargs):
+        try:
+            thisRecord = HomePageText.objects.get(id=id)
+            serializer = HomePageTextSerializer(thisRecord, data=request.data, partial=True)
+            
+            if serializer.is_valid():
+                serializer.save()
+        except:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({}, status=status.HTTP_200_OK)   
