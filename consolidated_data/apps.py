af414eb497944ef8a8ed3f37e0e44c71c2aa1d55
nursestacey <stacey.melnick.np@gmail.com>
2026-04-20 07:11:59 -0700

    update

--- consolidated_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConsolidatedDataConfig(AppConfig):
-    name = 'consolidated_data'
--- consolidated_data/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from gameresults.models import GameResultModel
-
-class PullDataForPointsSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = GameResultModel
-        fields ='__all__'        
